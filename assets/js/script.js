cf22a018fc50b7b2afbaa346be53347ac20cf75b
Sean Cone <seandoesitbetter@gmail.com>
2020-06-14 14:00:23 -0700

    Adding more code

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,39 @@ generateBtn.addEventListener("click", writePassword);
 
 // starting here
 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+
 const randomFunc = {
   lower: getRandomLower;
   upper: getRandomUpper;
   number: getRandomNumber;
   symbol: getRandomSymbol;
+};
 
+generateEl.addEventListener('click', () => {
+  const length = lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innertext = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  
 }
 
 function getRandomLower() {

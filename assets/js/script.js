95154259e4fb8b381f93431c33e97d945b366caf
Sean Cone <seandoesitbetter@gmail.com>
2020-06-14 12:27:47 -0700

    Adding functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// starting here
+
+const randomFunc = {
+  lower: getRandomLower;
+  upper: getRandomUpper;
+  number: getRandomNumber;
+  symbol: getRandomSymbol;
+
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}

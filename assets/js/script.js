705832acbb5f4a89a1d507481535873ba9738bf8
Sean Cone <seandoesitbetter@gmail.com>
2020-06-14 16:49:23 -0700

    Refactoring code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-const resultEl = document.getElementById('result');
+const resultEl = document.getElementById('password');
 const lengthEl = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
@@ -8,58 +8,51 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 
 const randomFunc = {
-  lower: getRandomLower;
-  upper: getRandomUpper;
-  number: getRandomNumber;
-  symbol: getRandomSymbol;
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
 };
 
-var length = window.prompt("Enter a number from 8 to 128 for password length.");
-var lowercase = window.confirm("Would you like to use lowercase letters?");
-var uppercase = window.confirm("Would you like to use uppercase letters?");
-var symbols = window.confirm("Would you like to use symbols?");
-var numbers = window.confirm("Would you like to use numbers?");
-
-generateEl.addEventListener('click', () => {
-  const length = +lengthEl.value;
-  const hasLower = lowercaseEl.prompt;
-  const hasUpper = uppercaseEl.prompt;
-  const hasNumber = numbersEl.prompt;
-  const hasSymbol = symbolsEl.prompt;
-
-  resultEl.innertext = generatePassword(
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol,
+  resultEl.innerText = generatePassword(
+    lowercase,
+    uppercase,
+    numbers,
+    symbols,
     length
   );
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
+
   let generatedPassword = '';
 
-  const typesCount = lower + upper + number + symbol;
 
-  constTypesArr = [{lower}, {upper}, {number}, {symbol}].filter(
-    item => Object.values(item)[0]
-    );
+  // Loop through some count of characters from i = 0 to i < length.
+  
+  for (let i = 0; i < length; i++) {
+    // For each time we loop, we want to pick a random char based on whether
+    // we are allowing lower, upper, numbers, or symbols.
 
-    if(typesCount === 0) {
-      return '';
-    }
+    // Start with empty.
+    listOfPossibleChars = [];
 
-    for(let i = 0; i < length; i += typesCount) {
-      typesArr.forEach(type => {
-        const funcName = Object.keys(type)[0];
 
-        generatePassword += randomFunc[funcName]();
-      });
-    }
 
-    const finalPassword = generatedPassword.slice(0, length);
+    // Randomize the list.
+    var randomListOfPossibleChars = listOfPossibleChars.sort(function () {
+      return Math.random - 0.5;
+    });
 
-    return finalPassword;
+    // Pick one from the list.
+    var randomChar = randomListOfPossibleChars.pop();
+
+    generatedPassword = generatedPassword + randomChar;
+  }
+
+  console.log(generatedPassword);
+
+  return generatedPassword;
 }
 
 function getRandomLower() {
@@ -84,12 +77,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var length = window.prompt("Enter a number from 8 to 128 for password length.");
+  var lowercase = window.confirm("Would you like to use lowercase letters?");
+  var uppercase = window.confirm("Would you like to use uppercase letters?");
+  var symbols = window.confirm("Would you like to use symbols?");
+  var numbers = window.confirm("Would you like to use numbers?");
+
+  var password = generatePassword(lowercase, uppercase, numbers, symbols, length);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generateEl.addEventListener('click', function () {
+//   var length = window.prompt("Enter a number from 8 to 128 for password length.");
+//   var lowercase = window.confirm("Would you like to use lowercase letters?");
+//   var uppercase = window.confirm("Would you like to use uppercase letters?");
+//   var symbols = window.confirm("Would you like to use symbols?");
+//   var numbers = window.confirm("Would you like to use numbers?");

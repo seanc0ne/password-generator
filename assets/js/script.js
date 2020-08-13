3a9091c15b616f49c8cde86a9e8832b8d75ba1d2
Sean Cone <seandoesitbetter@gmail.com>
2020-08-12 18:56:38 -0700

    Redoing assignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,105 @@
-// Assignment code here
-const resultEl = document.getElementById('password');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
+const generateBtn = document.querySelector('#generate');
+const copyBtn = document.querySelector('#copy');
+let passwordText = document.querySelector('#password');
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let specialCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '?', '@', ',', '-', '.', '/', ':', ';', '<', '>', '=', '[', ']', '\\', '^', '_', '`', '{', '}', '|', '~'];
 
-  resultEl.innerText = generatePassword(
-    lowercase,
-    uppercase,
-    numbers,
-    symbols,
-    length
-  );
-});
+let randomPassword = [];
 
-function generatePassword(lower, upper, number, symbol, length) {
+// // Assignment code here
+// const resultEl = document.getElementById('password');
+// const lengthEl = document.getElementById('length');
+// const uppercaseEl = document.getElementById('uppercase');
+// const lowercaseEl = document.getElementById('lowercase');
+// const numbersEl = document.getElementById('numbers');
+// const symbolsEl = document.getElementById('symbols');
+// const generateEl = document.getElementById('generate');
 
-  let generatedPassword = '';
-  for (let i = 0; i < length; i++) {
-    listOfPossibleChars = [];
-    if (lower) {
-      listOfPossibleChars.push(getRandomLower())
-    }
-    if (upper) {
-      listOfPossibleChars.push(getRandomUpper())
-    }
-    if (number) {
-      listOfPossibleChars.push(getRandomNumber())
-    }
-    if (symbol) {
-      listOfPossibleChars.push(getRandomSymbol())
-    }
+// const randomFunc = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbol
+// };
 
-    var randomListOfPossibleChars = listOfPossibleChars.sort(function () {
-      return Math.random - 0.5;
-    });
+//   resultEl.innerText = generatePassword(
+//     lowercase,
+//     uppercase,
+//     numbers,
+//     symbols,
+//     length
+//   );
+// });
 
-    var randomChar = randomListOfPossibleChars.pop();
+// function generatePassword(lower, upper, number, symbol, length) {
 
-    generatedPassword = generatedPassword + randomChar;
-  }
+//   let generatedPassword = '';
+//   for (let i = 0; i < length; i++) {
+//     listOfPossibleChars = [];
+//     if (lower) {
+//       listOfPossibleChars.push(getRandomLower())
+//     }
+//     if (upper) {
+//       listOfPossibleChars.push(getRandomUpper())
+//     }
+//     if (number) {
+//       listOfPossibleChars.push(getRandomNumber())
+//     }
+//     if (symbol) {
+//       listOfPossibleChars.push(getRandomSymbol())
+//     }
 
-  console.log(generatedPassword);
+//     var randomListOfPossibleChars = listOfPossibleChars.sort(function () {
+//       return Math.random - 0.5;
+//     });
 
-  return generatedPassword;
-}
+//     var randomChar = randomListOfPossibleChars.pop();
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+//     generatedPassword = generatedPassword + randomChar;
+//   }
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+//   console.log(generatedPassword);
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+//   return generatedPassword;
+// }
 
-function getRandomSymbol() {
-  const symbols = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+// function getRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
 
-// Write password to the #password input
-function writePassword() {
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
 
-  var length = window.prompt("Enter a number from 8 to 128 for password length.");
-  var lowercase = window.confirm("Would you like to use lowercase letters?");
-  var uppercase = window.confirm("Would you like to use uppercase letters?");
-  var symbols = window.confirm("Would you like to use symbols?");
-  var numbers = window.confirm("Would you like to use numbers?");
+// function getRandomSymbol() {
+//   const symbols = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// }
 
-  var password = generatePassword(lowercase, uppercase, numbers, symbols, length);
-  var passwordText = document.querySelector("#password");
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
+// // Write password to the #password input
+// function writePassword() {
 
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//   var length = window.prompt("Enter a number from 8 to 128 for password length.");
+//   var lowercase = window.confirm("Would you like to use lowercase letters?");
+//   var uppercase = window.confirm("Would you like to use uppercase letters?");
+//   var symbols = window.confirm("Would you like to use symbols?");
+//   var numbers = window.confirm("Would you like to use numbers?");
+
+//   var password = generatePassword(lowercase, uppercase, numbers, symbols, length);
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);

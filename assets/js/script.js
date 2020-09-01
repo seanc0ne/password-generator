2efe6b8910d1f53eb41448de139ccaf745106da6
Sean Cone <seandoesitbetter@gmail.com>
2020-08-31 17:38:39 -0700

    Finishing refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ const copyBtn = document.querySelector('#copy');
 let passwordText = document.querySelector('#password');
 
 //character arrays
-let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let specialCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '?', '@', ',', '-', '.', '/', ':', ';', '<', '>', '=', '[', ']', '\\', '^', '_', '`', '{', '}', '|', '~'];
 
@@ -32,8 +32,40 @@ function writePassword() {
     let numberQuestion = confirm('Would you like the password to include numbers? Click OK for yes, cancel for no.');
     let lowercaseQuestion = confirm('Would you like the password to include lowercase characters? Click OK for yes, cancel for no.');
     let uppercaseQuestion = confirm('Would you like the password to include uppercase characters? Click OK for yes, cancel for no.');
+
+    if(specialCharactersQuestion) {
+        randomPassword.push(specialCharacters);
+    }
+    if(numberQuestion) {
+        randomPassword.push(numbers);
+    }
+    if(lowercaseQuestion) {
+        randomPassword.push(lowercase);
+    }
+    if(uppercaseQuestion) {
+        randomPassword.push(uppercase);
+    }
+
+    else if (!specialCharactersQuestion && !numberQuestion && !lowercaseQuestion && !uppercaseQuestion) {
+        alert('Please select at least one character type.')
+        return;
+    }
+
+    let password = generatePassword(randomPassword, numberOfCharacters);
+
+    passwordText.value = password;
 }
 
+function copyToClipboard() { 
+    let copyPass = document.querySelector('#password');
+    copyPass.querySelector();
+    document.execCommand("copy");
+    alert(`The password ${copyPass.value} has been copied to your clipboard.`)
+}
+
+generateBtn.addEventListener('click', writePassword);
+
+copyBtn.addEventListener('click', copyToClipboard);
 
 // // Assignment code here
 // const resultEl = document.getElementById('password');
